29ba1585323d2168db9dec1c4073d67a3b0450cb
PVIdubs <nluismi80@gmail.com>
2023-03-24 15:26:32 -0400

    user-schema

---
+++ models/User.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    lowercase: true,
+    index: { unique: true },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
