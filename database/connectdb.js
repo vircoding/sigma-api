6ea53d3ad3376ad285766b466796a3724ca38b6c
PVIdubs <nluismi80@gmail.com>
2023-03-24 14:45:30 -0400

    connecting to db

---
+++ database/connectdb.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+try {
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log("MongoDB connection established 😋");
+} catch (error) {
+  console.log("MongoDB connection error 😥: " + error);
+}
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import "dotenv/config";
+import "./database/connectdb.js";
 
 const app = express();
 
+// Starting Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server started on http://localhost:${PORT}; press Ctrl-C to terminate.`)
