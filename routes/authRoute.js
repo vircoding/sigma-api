1b198937a2288764a95d831bdbb32995ab623af8
PVIdubs <nluismi80@gmail.com>
2023-03-26 23:03:34 -0400

    minor changes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { register, login } from "../controllers/authControllers.js";
+import { register, login } from "../controllers/authController.js";
 import { body } from "express-validator";
 import { valResults } from "../middlewares/valResults.js";
 
