b4fde514638ca0fa1b0dd64be197cb897b7930d4
PVIdubs <nluismi80@gmail.com>
2023-04-01 07:51:10 -0400

    minor changes

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 import express from "express";
 import "dotenv/config";
 import "./database/connectdb.js";
-import cors from "cors";
 import cookieParser from "cookie-parser";
-
 import authRouter from "./routes/authRoute.js";
 
 const app = express();
 
-const whiteList = [process.env.ORIGIN1, process.env.ORIGIN2];
-
 // Middlewares
+app.use("/api/v1/auth", authRouter);
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors());
-
-app.use("/api/v1/auth", authRouter);
 
 // Starting Server
 const PORT = process.env.PORT || 5000;
