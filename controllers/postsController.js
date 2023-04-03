507bb2f0dc5e28271a9a1270e71e2d972b912b76
PVIdubs <nluismi80@gmail.com>
2023-04-03 15:56:21 -0400

    set-post route

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -9,3 +9,15 @@ export const getPosts = async (req, res) => {
     return res.status(500).json({ error: "Server error" });
   }
 };
+
+export const setPost = async (req, res) => {
+  try {
+    const { buy, price } = req.body;
+    const post = new Post({ buy, price, uid: req.uid });
+    const newPost = await post.save();
+
+    return res.json(newPost);
+  } catch (error) {
+    return res.status(500).json({ error: "Server error" });
+  }
+};
--- routes/postsRoute.js
+++ routes/postsRoute.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getPosts } from "../controllers/postsController.js";
+import { getPosts, setPost } from "../controllers/postsController.js";
 import { requireToken } from "../middlewares/requireToken.js";
 
 const router = Router();
 
 router.get("/", requireToken, getPosts);
+router.post("/", requireToken, setPost);
 
 export default router;
