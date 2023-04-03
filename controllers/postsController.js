98b8a9b8105e98c8204e22e3b43c4a965c468752
PVIdubs <nluismi80@gmail.com>
2023-04-03 19:33:44 -0400

    remove-post route

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -35,3 +35,22 @@ export const setPost = async (req, res) => {
     return res.status(500).json({ error: "Server error" });
   }
 };
+
+export const removePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Post.findById(id);
+
+    if (!post) return res.status(404).json({ error: "No existe el post" });
+
+    if (!post.uid.equals(req.uid))
+      return res.status(401).json({ error: "No le pertenece ese id 🤡" });
+
+    await post.deleteOne();
+
+    return res.json({ post });
+  } catch (error) {
+    if (error.kind === "ObjectId") return res.status(403).json({ error: "non-valid Post ID" });
+    return res.status(500).json({ error: "Server error" });
+  }
+};
--- routes/postsRoute.js
+++ routes/postsRoute.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getPosts, getPost, setPost } from "../controllers/postsController.js";
+import { getPosts, getPost, setPost, removePost } from "../controllers/postsController.js";
 import { requireToken } from "../middlewares/requireToken.js";
 import { postValidator } from "../middlewares/requestValidator.js";
 
@@ -8,5 +8,6 @@ const router = Router();
 router.get("/", requireToken, getPosts);
 router.get("/:id", requireToken, getPost);
 router.post("/", requireToken, postValidator, setPost);
+router.delete("/:id", requireToken, removePost);
 
 export default router;
