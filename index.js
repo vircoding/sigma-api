02f48688c169b447b6901ad1cd850ec33a1e04b8
PVIdubs <nluismi80@gmail.com>
2023-04-06 02:37:42 -0400

    cors

--- index.js
+++ index.js
@@ -4,10 +4,23 @@ import "./database/connectdb.js";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/authRoute.js";
 import postsRouter from "./routes/postsRoute.js";
+import cors from "cors";
 
 const app = express();
 
+const whiteList = [process.env.ORIGIN];
+
 // Middlewares
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (whiteList.includes(origin)) {
+        return callback(null, origin);
+      }
+      return callback("CORS Origin Error: " + origin);
+    },
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/auth", authRouter);
