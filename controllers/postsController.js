09552518c1ff8aef98bc000faee726fbf48e7235
PVIdubs <nluismi80@gmail.com>
2023-10-06 16:15:04 -0400

    improving posts-route

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,4 +1,5 @@
 import { Post } from "../models/Post.js";
+import { User } from "../models/User.js";
 import { Sale } from "../models/Sale.js";
 import { Rent } from "../models/Rent.js";
 import { Exchange } from "../models/Exchange.js";
@@ -132,6 +133,192 @@ export const visitPost = async (req, res) => {
     return res.json({ ok: true });
   } catch (error) {
     if (error.kind === "ObjectId") return res.status(403).json({ error: "non-valid Post ID" });
+    console.log(error);
+    return res.status(500).json({ error: "Server error" });
+  }
+};
+
+export const getUserPosts = async () => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const posts = [];
+  try {
+    const user = await User.findById(req.uid);
+    if (!user) return res.status(404).json({ error: "User not founded" });
+
+    user.posts.slice((page - 1) * limit, page * limit).map(async (item) => {
+      const post = await Post.findById(item);
+
+      if (!post) return res.status(404).json({ error: "Post not founded", post_id: item });
+      posts.push(post);
+    });
+
+    const total_posts = user.posts.length;
+
+    return res.json({
+      posts: posts.map((item) => {
+        return formatPostRes(item);
+      }),
+      page: total_posts === 0 ? 0 : page,
+      total_posts,
+      total_pages: Math.ceil(total_posts / limit),
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Server error" });
+  }
+};
+
+export const getUserFavorites = async () => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const favorites = [];
+  try {
+    const user = await User.findById(req.uid);
+    if (!user) return res.status(404).json({ error: "User not founded" });
+
+    user.favorites.slice((page - 1) * limit, page * limit).map(async (item) => {
+      if (item.status === "active") {
+        const post = await Post.findById(item.post_id);
+        if (!post) return res.status(404).json({ error: "Post not founded", post_id: item });
+        favorites.push(post);
+      } else {
+        favorites.push({ id: item.post_id });
+      }
+    });
+
+    const total_favorites = user.favorites.length;
+
+    return res.json({
+      favorites: user.favorites.map((item, index) => {
+        if (item.status === "active") return formatPostRes(favorites[index]);
+        else return favorites[index];
+      }),
+      page: total_favorites === 0 ? 0 : page,
+      total_favorites,
+      total_pages: Math.ceil(total_favorites / limit),
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Server error" });
+  }
+};
+
+export const getPopularPosts = async () => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+
+  try {
+    const popular = await Post.aggregate([
+      { $group: { _id: "$meta.visit_count", posts: { $push: "$$ROOT" } } },
+      { $sort: { _id: -1 } },
+      { $skip: (page - 1) * limit },
+      { $limit: 10 },
+      { $unwind: "$posts" },
+      { $replaceRoot: { newRoot: "$posts" } },
+    ]);
+
+    const total_posts = await Post.find().countDocuments();
+
+    return res.json({
+      posts: popular.map((item) => {
+        return formatPostRes(item);
+      }),
+      page: total_posts === 0 ? 0 : page,
+      total_posts,
+      total_pages: Math.ceil(total_posts / limit),
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Server error" });
+  }
+};
+
+export const getPopularSales = async () => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+
+  try {
+    const popular = await Sale.aggregate([
+      { $group: { _id: "$meta.visit_count", sales: { $push: "$$ROOT" } } },
+      { $sort: { _id: -1 } },
+      { $skip: (page - 1) * limit },
+      { $limit: 10 },
+      { $unwind: "$sales" },
+      { $replaceRoot: { newRoot: "$sales" } },
+    ]);
+
+    const total_sales = await Sale.find().countDocuments();
+
+    return res.json({
+      sales: popular.map((item) => {
+        return formatPostRes(item);
+      }),
+      page: total_sales === 0 ? 0 : page,
+      total_sales,
+      total_pages: Math.ceil(total_sales / limit),
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Server error" });
+  }
+};
+
+export const getPopularRents = async () => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+
+  try {
+    const popular = await Rent.aggregate([
+      { $group: { _id: "$meta.visit_count", rents: { $push: "$$ROOT" } } },
+      { $sort: { _id: -1 } },
+      { $skip: (page - 1) * limit },
+      { $limit: 10 },
+      { $unwind: "$rents" },
+      { $replaceRoot: { newRoot: "$rents" } },
+    ]);
+
+    const total_rents = await Rent.find().countDocuments();
+
+    return res.json({
+      rents: popular.map((item) => {
+        return formatPostRes(item);
+      }),
+      page: total_rents === 0 ? 0 : page,
+      total_rents,
+      total_pages: Math.ceil(total_rents / limit),
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Server error" });
+  }
+};
+
+export const getPopularExchanges = async () => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+
+  try {
+    const popular = await Exchange.aggregate([
+      { $group: { _id: "$meta.visit_count", exchanges: { $push: "$$ROOT" } } },
+      { $sort: { _id: -1 } },
+      { $skip: (page - 1) * limit },
+      { $limit: 10 },
+      { $unwind: "$exchanges" },
+      { $replaceRoot: { newRoot: "$exchanges" } },
+    ]);
+
+    const total_exchanges = await Exchange.find().countDocuments();
+
+    return res.json({
+      exchanges: popular.map((item) => {
+        return formatPostRes(item);
+      }),
+      page: total_exchanges === 0 ? 0 : page,
+      total_exchanges,
+      total_pages: Math.ceil(total_exchanges / limit),
+    });
+  } catch (error) {
     console.log(error);
     return res.status(500).json({ error: "Server error" });
   }
--- routes/postsRoute.js
+++ routes/postsRoute.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
+import { requireToken } from "../middlewares/requireToken.js";
 import {
   getPosts,
   getSales,
   getRents,
   getExchanges,
   getPost,
+  getUserPosts,
+  getUserFavorites,
+  getPopularPosts,
   visitPost,
 } from "../controllers/postsController.js";
 import { paramValidator } from "../middlewares/requestValidator.js";
@@ -17,10 +21,12 @@ router.get("/sales", getSales); // Get All Sales
 router.get("/rents", getRents); // Get All Rents
 router.get("/exchanges", getExchanges); // Get All Exchanges
 router.get("/:id", paramValidator, getPost); // Get Post
-// router.get("/popular", getPopularPosts); // Get All Popular
-// router.get("/popular/sales", getPopularSales); // Get Popular Sales
-// router.get("/popular/rents", getPopularRents); // Get Popular Rents
-// router.get("/popular/exchanges", getPopularExchanges); // Get Popular Exchanges
+router.get("/user", requireToken, getUserPosts); // Get User Posts
+router.get("/user/favorites", requireToken, getUserFavorites); // Get User Favorites
+router.get("/popular", getPopularPosts); // Get All Popular
+router.get("/popular/sales", getPopularSales); // Get Popular Sales
+router.get("/popular/rents", getPopularRents); // Get Popular Rents
+router.get("/popular/exchanges", getPopularExchanges); // Get Popular Exchanges
 
 // PUT
 router.put("/:id", paramValidator, visitPost); // Visit Post
